294153c99d36ade0e08790f8e425ffc8289a728b
juhy-git <libor.juhanak@gmail.com>
2016-06-05 21:23:48 +0200

    H14

--- H14_06_create_dataset_pedagogika.R
+++ H14_06_create_dataset_pedagogika.R
@@ -2,6 +2,9 @@ data_all <- read.csv("./data/dataset_Pil1-filtered.csv", stringsAsFactors=FALSE)
 data_ped <- data_all[data_all$CR_OBR == "AM - Pedagogika a školství", ]
 rm(data_all)
 
+
+
+
 # Je třeba vyřešit duplicity na výzkumných organizacích a u autorů publikací
 # - AUTOŘI: Stává se, že u též publikace jsou uvádění různé kombinace autořů.
 #           Nejjednodušší by bylo vzít prostě ten nejdelší řetězec autorů.
@@ -17,6 +20,29 @@ data_ped <- data_ped[order(data_ped$VYSNID, data_ped$VYSBOD, -data_ped$VYSPDL), 
 # Zbavení se duplikací podle vnitřního ID
 data_ped <- data_ped[!duplicated(data_ped$VYSNID), ]
 
+
+
+
+# Je třeba provést základní čištění dat:
+# - Opravit časopisy alespoň v kategriích: Jimp, Jsc, Jneimp, Jrec
+# - Opravit nakladatele (sloupec NAKNAZ)
+
+j_opravy <- read.csv("./data_ped/j_opravy_nazvu.csv", stringsAsFactors=FALSE)
+data_ped$DOKNAZPRI <- tolower(data_ped$DOKNAZPRI)
+
+for (i in 1:nrow(data_ped)){
+    for (j in 1:nrow(j_opravy)){
+        if (data_ped$DOKNAZPRI[i] == j_opravy$nazev_k_oprave[j]) {
+            
+            data_ped$DOKNAZPRI[i] <- j_opravy$spravny_nazev[j]
+        
+        }
+    }
+}
+
+
 # Uložení hotového "čistého" souboru pro pedagogiku
 write.csv(data_ped, file = "./data_ped/data_ped.csv", row.names = FALSE)
+
+
 
